0dbbdc26a7a8ab872730167d412510806b7cce68
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-05-22 13:23:11 +0200

    refactor: changed the button from the header into its own div (Since I want it to be above the title) and changed the class from dialog-header to dialog-close-button. Then put the dialog h2 in the <header> for improved semantics.

--- JS/script.js
+++ JS/script.js
@@ -89,7 +89,9 @@ function removeOpenedClass() {
 
 function getImgTemplate(currentIndex) {
     return `<div class="single_element">
+        <header>
         <h2 id="dialog-image-title">${currentTitles[currentIndex]}</h2>
+        </header>
         <img class="dialog-img" src="./img/dialog/500kb/${myImg[currentIndex]}" alt="${myAlts[currentIndex]}">
         <footer class="dialog-navigation"> 
         <button onclick="previousImg()" id="previous-img" aria-label="Previous image"><img src="./img/button_left.svg" alt="Arrow left"></button>
